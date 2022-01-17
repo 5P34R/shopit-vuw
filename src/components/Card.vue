d7375fc74b14ed12a908478a809fb26707e26b42
VishDroid-dev <vk2345732@gmail.com>
2022-01-17 14:28:49 +0530

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+   <div class="max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="w-full" :src="image" alt="">
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">{{title}}</div>
+    <p class="text-gray-700 text-base">
+     {{description}}
+    </p>
+  </div>
+  <div class="px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+    name:"Card",
+    props: ['title', 'description', 'image']
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mx-20">
+    <Card title="Its my heading" description="Lorel ip"/>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Card from '../components/Card.vue'
 
 export default {
   name: 'Home',
+  data(){
+    return {
+      data: []
+    }
+  },
   components: {
-    HelloWorld
+    Card
+  },
+  methods:{
+    async getData(){
+      await axios.get(`http://127.0.0.1:8000/`).then((res) => {
+        res.data.map((e) => {
+          this.data.append(e)
+        })
+      })
+    }
+  },
+  mounted(){
+    this.getData()
   }
 }
 </script>
