48f060802ccca40286a508c1e5eeb1e5a9335705
VishDroid-dev <vk2345732@gmail.com>
2022-01-17 20:24:43 +0530

    added navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,9 +12,13 @@
         </div>
 
         <!-- search -->
-        <div class="flex max-w-full my-3 justify-center bg-gray-600 rounded-lg focus:outline-none">
-            <input v-model="search" type="text" placeholder="Search here...">
-            <button type="submit" @click="Search">Button</button>
+        <div class="flex max-w-full my-3 justify-center rounded-lg">
+           <form class="group relative">
+      <svg width="20" height="20" fill="currentColor" class="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-500" aria-hidden="true">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
+      </svg>
+      <input class="focus:ring-2 focus:ring-purple-400 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Filter projects" placeholder="Filter projects...">
+    </form>
         </div>
 
         <!-- options -->
